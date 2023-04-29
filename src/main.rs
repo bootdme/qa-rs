f30ca38f10df6575282200f28c342a42240c350d
David Estephanian <desteph@protonmail.com>
2023-04-28 22:02:27 -0700

    Added inital cargo dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
-use diesel::pg::PgConnection;
-use diesel::r2d2::{self, ConnectionManager};
-use dotenv::dotenv;
-use hyper::{Body, Request, Response, Server};
-use routerify::{Middleware, RequestInfo, Router};
-use std::convert::Infallible;
-use std::env;
-
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
+    println!("Hello world");
 }
