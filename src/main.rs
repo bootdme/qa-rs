7469d6205056959babf6627199610028fb44364d
David Estephanian <desteph@protonmail.com>
2023-05-03 23:48:45 -0700

    Added necessary comments to main

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,18 @@ use hyper::{
 use sqlx::postgres::PgPoolOptions;
 use std::{net::SocketAddr, sync::Arc};
 
+/// The entry point for the application.
+///
+/// This function initializes the environment variables, creates a connection pool
+/// to the database, sets up the server to listen on a specific address, and
+/// starts the server.
+///
+/// # Errors
+///
+/// Returns an error if any of the following occurs:
+/// - Failed to load environment variables
+/// - Failed to create a connection pool to the database
+/// - Failed to bind the server to the specified address
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     dotenv().ok();
@@ -20,12 +32,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let database_url = std::env::var("DATABASE_URL")?;
 
     let pool = PgPoolOptions::new().max_connections(5).connect(&database_url).await?;
+
+    // Wrap the connection pool in an Arc for shared ownership and thread safety
     let pool = Arc::new(pool);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
+    // Create a service factory function that handles incoming connections
     let make_svc = make_service_fn(move |_conn: &AddrStream| {
+        // Clone the connection pool for each incoming connection
         let pool = pool.clone();
+
+        // Return a service function that handles incoming requests and passes them to the router
         async { Ok::<_, hyper::Error>(service_fn(move |req| routes::handle_request(pool.clone(), req))) }
     });
 
